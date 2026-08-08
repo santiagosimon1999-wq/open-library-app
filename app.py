83224f5bdd53bd814704ffcb05379d80de762c9c
santiagosimon1999-wq <santiagosimon1999@gmail.com>
2026-08-08 16:00:53 -0400

    Add book covers and improve result layout

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ def searchbooks(query):
 
 st.title("Open Library Book Search")
 
-st.write("My first Streamlit application!")
-
 query = st.text_input("Search for a book")
 
 if st.button("Search"):
@@ -25,7 +23,27 @@ if st.button("Search"):
         data = searchbooks(query)
 
         for book in data.get("docs", []):
-            st.write(f"Title: {book.get('title', 'N/A')}")
-            st.write(f"Author: {book.get('author_name', ['N/A'])[0]}")
-            st.write(f"Year: {book.get('first_publish_year', 'N/A')}")
-            st.write("---")
+            title = book.get("title", "Unknown")
+
+            authors = book.get("author_name", [])
+            author = authors[0] if authors else "Unknown"
+
+            year = book.get("first_publish_year", "Unknown")
+
+            cover_id = book.get("cover_i")
+
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                if cover_id:
+                    cover_url = f"https://covers.openlibrary.org/b/id/{cover_id}-M.jpg"
+                    st.image(cover_url, width=120)
+                else:
+                    st.write("No cover")
+
+                with col2:
+                    st.write(f"Title: {title}")
+                    st.write(f"Author: {author}")
+                    st.write(f"First Published: {year}")
+
+                    st.write("---")
